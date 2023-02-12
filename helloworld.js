7f2382cbd9f0775de36415e1a184591d7d69fa48
Kim Uy Computer <Kim Uy Computer@VATHANAK>
2023-02-12 14:03:18 +0700

    initial commit

--- helloworld.js
+++ helloworld.js
@@ -3,16 +3,14 @@ var arr = ["Piseth","Kimseng","Sochiva","Mean","Dy","Vathanak"]; //6 elements
 // var t1 = arr[0];
 // var t2 = arr[4];
 // console.log("My name is " + arr[1] + ". And his classmate's name is " + arr[3] + ".");
-arr[2] = "Binary";
-console.log(arr[5-3]);
+// arr[2] = "Binary";
+// console.log(arr[5-3]);
 var arr2 = [20,66,33,12,80,"72"];
 // console.log(arr2[0]);
 // console.log(arr2[arr2.length-1]);
 // console.log(arr2[2] + arr2[3]); //45
-delete arr2[arr2.length-1];
-console.log(arr2);
-arr2.push();
-console.log(arr2);
+// delete arr2[arr2.length-1];
+// console.log(arr2);
 // arr2[6] = 100;
 // console.log(arr2[arr2.length-1]);
 var arr3 = ["string",22,50,"coca","23","28"];
@@ -25,5 +23,17 @@ var arr3 = ["string",22,50,"coca","23","28"];
 var arr4 = [20,23];
 // var t1 = arr4[0].toString() + arr4[1].toString();
 // console.log(t1);
+function word(word){
+    console.log("I like number " + word);
+}
+function word2(num,word){
+    console.log("I love "+word);
+    console.log(5+num);
+}
+function multi(num1,num2){
+    var result = num1*num2;
+    console.log(result);
+}
+multi(5,3);
 
 
