c146f21f6ed9edcfa60a342a4ac39cc31ba9ebd8
Kim Uy Computer <Kim Uy Computer@VATHANAK>
2023-02-15 10:30:43 +0700

    initial commit

--- helloworld.js
+++ helloworld.js
@@ -1,39 +1,9 @@
-var arr = ["Piseth","Kimseng","Sochiva","Mean","Dy","Vathanak"]; //6 elements
-//console.log(arr);
-// var t1 = arr[0];
-// var t2 = arr[4];
-// console.log("My name is " + arr[1] + ". And his classmate's name is " + arr[3] + ".");
-// arr[2] = "Binary";
-// console.log(arr[5-3]);
-var arr2 = [20,66,33,12,80,"72"];
-// console.log(arr2[0]);
-// console.log(arr2[arr2.length-1]);
-// console.log(arr2[2] + arr2[3]); //45
-// delete arr2[arr2.length-1];
-// console.log(arr2);
-// arr2[6] = 100;
-// console.log(arr2[arr2.length-1]);
-var arr3 = ["string",22,50,"coca","23","28"];
-// console.log(arr3[0] + " " + arr3[1]);
-// console.log(arr3[1] + arr3[2]);
-// console.log(arr3[2] + parseInt(arr3[arr3.length-1]));
-// console.log(arr[arr.length-1] + " would like to drink " + arr3[arr3.length-2]); 
-// var result = parseInt(arr2[arr2.length-1]) + parseInt(arr3[arr3.length-1]);
-// console.log("Number " + arr2[arr2.length-1] + " plus " + arr3[arr3.length-1] + " equal " + result);
-var arr4 = [20,23];
-// var t1 = arr4[0].toString() + arr4[1].toString();
-// console.log(t1);
-function word(word){
-    console.log("I like number " + word);
-}
-function word2(num,word){
-    console.log("I love "+word);
-    console.log(5+num);
-}
-function multi(num1,num2){
-    var result = num1*num2;
-    console.log(result);
-}
-multi(5,3);
+console.log("Welcome to our application");
+var pass = prompt("Create password: ");
+console.log("You created a password successfully");
+var login = prompt("Enter password: ");
 
-
+while(pass!=login){
+    login = prompt("Oh sorry! Your password is incorrect, please try again: ");
+}
+console.log("You are in");
---
+++ javaScript.js
@@ -0,0 +1,3 @@
+function myFunction(){
+    document.getElementById('demo').innerHTML = "Happy New Year 2023";
+}
