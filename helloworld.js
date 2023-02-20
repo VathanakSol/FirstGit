ae58d266c07c00725d0a46380870235328fcd426
Kim Uy Computer <Kim Uy Computer@VATHANAK>
2023-02-20 20:50:07 +0700

    initial commit

--- helloworld.js
+++ helloworld.js
@@ -1,9 +1,16 @@
-console.log("Welcome to our application");
-var pass = prompt("Create password: ");
-console.log("You created a password successfully");
-var login = prompt("Enter password: ");
+// console.log("Welcome to our application");
+// var pass = prompt("Create password: ");
+// console.log("You created a password successfully");
+// var login = prompt("Enter password: ");
 
-while(pass!=login){
-    login = prompt("Oh sorry! Your password is incorrect, please try again: ");
-}
-console.log("You are in");
+// while(pass!=login){
+//     login = prompt("Oh sorry! Your password is incorrect, please try again: ");
+// }
+// console.log("You are in");
+// var num = false;
+// if(num){
+//     console.log('True');
+// }else{
+//     console.log('False');
+// }
+
