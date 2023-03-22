461f4616aacc1df042ca4e571bea7f425678cd47
Kim Uy Computer <Kim Uy Computer@VATHANAK>
2023-03-22 11:32:18 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function show(){
+    var input1 = i1.valueAsText;
+    var input2 = i2.valueAsPassword;
+    var input3 = i3.valueAsNumber;
+    alert(input1);
+    alert(input2);
+    alert(input3);
+}
